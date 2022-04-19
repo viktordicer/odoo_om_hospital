cdd5d058fb6da1652114d2ca9b8885dc5fc7e0b5
viktor.dicer <viktor.dicer@gmail.com>
2022-04-19 21:28:12 +0200

    initial commit

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Hospital Management',
+    'version': '1.0.0',
+    'category': 'Hospital',
+    'summary': 'Hospital management system',
+    'description': """Hospital management system""",
+    'depends': [],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/menu.xml',
+        'views/patient_view.xml',
+        ],
+    'demo': [],
+    'application': True,
+    'auto_install': False,
+    'assets': {},
+    'license': 'LGPL-3',
+}
