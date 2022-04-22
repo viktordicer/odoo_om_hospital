96924ddd174379b117548489cc8c67526eeb158f
viktor.dicer <viktor.dicer@gmail.com>
2022-04-22 22:20:33 +0200

    24 create computed field

--- __manifest__.py
+++ __manifest__.py
@@ -6,11 +6,12 @@
     'category': 'Hospital',
     'summary': 'Hospital management system',
     'description': """Hospital management system""",
-    'depends': [],
+    'depends': ['mail',],
     'data': [
         'security/ir.model.access.csv',
         'views/menu.xml',
         'views/patient_view.xml',
+        'views/appointment_view.xml',
         'views/female_patient_view.xml',
         ],
     'demo': [],
--- models/patient.py
+++ models/patient.py
@@ -2,9 +2,10 @@ from odoo import api, fields, models
 
 class HospitalPatient(models.Model):
   _name = "hospital.patient"
+  _inherit = ['mail.thread', 'mail.activity.mixin']
   _description = "Hospital Patient"
   
-  name = fields.Char(string='Name')
+  name = fields.Char(string='Name', tracking=True)
   ref = fields.Char(string='Reference')
   age = fields.Integer(string="Age")
   gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string="Gender")
