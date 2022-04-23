49cb555719721e1addc8d06fca1ef1632869d3c4
viktor.dicer <viktor.dicer@gmail.com>
2022-04-23 21:40:57 +0200

    30

--- models/appointment.py
+++ models/appointment.py
@@ -4,8 +4,15 @@ class HospitalAppointment(models.Model):
   _name = "hospital.appointment"
   _inherit = ['mail.thread', 'mail.activity.mixin']
   _description ="Hospital Appointment"
+  _rec_name = 'ref'
   
   patient_id = fields.Many2one('hospital.patient', string="Patient")
   gender = fields.Selection(related='patient_id.gender')
   appointment_time = fields.Datetime(string='Appointment Time', default=fields.Datetime.now)
   booking_date = fields.Date(string='Booking Date',  default=fields.Date.context_today)
+  ref = fields.Char(string='Reference')
+  prescription = fields.Html(string='Prescription')
+  
+  @api.onchange('patient_id')
+  def onchange_patient_id(self):
+    self.ref = self.patient_id.ref
