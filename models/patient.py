f5f3d777918ffa013311c8df283c692418e93401
viktor.dicer <viktor.dicer@gmail.com>
2022-04-23 21:41:06 +0200

    30

--- models/patient.py
+++ models/patient.py
@@ -1,3 +1,4 @@
+from datetime import date
 from odoo import api, fields, models
 
 class HospitalPatient(models.Model):
@@ -6,7 +7,17 @@ class HospitalPatient(models.Model):
   _description = "Hospital Patient"
   
   name = fields.Char(string='Name', tracking=True)
+  date_of_birth = fields.Date(string='Date Of Birth', tracking=True)
   ref = fields.Char(string='Reference')
-  age = fields.Integer(string="Age")
+  age = fields.Integer(string="Age", compute='_compute_age')
   gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string="Gender")
   active = fields.Boolean(string="Active", default=True)
+  
+  @api.depends('date_of_birth')
+  def _compute_age(self):
+    for rec in self:
+      today = date.today()
+      if rec.date_of_birth:
+        rec.age = today.year - rec.date_of_birth.year - ((today.month, today.day) < (rec.date_of_birth.month, rec.date_of_birth.day))
+      else:
+        rec.age = 0
