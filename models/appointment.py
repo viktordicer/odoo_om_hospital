ff4c631a9f3e6c5085f37a754626992727079882
viktor.dicer <viktor.dicer@gmail.com>
2022-04-24 21:42:47 +0200

    54 lesson

--- models/appointment.py
+++ models/appointment.py
@@ -12,7 +12,47 @@ class HospitalAppointment(models.Model):
   booking_date = fields.Date(string='Booking Date',  default=fields.Date.context_today)
   ref = fields.Char(string='Reference')
   prescription = fields.Html(string='Prescription')
+  priority = fields.Selection([
+    ('0', 'Normal'),
+    ('1', 'Low'),
+    ('2', 'High'),
+    ('3', 'Very High')], string='Priority')
+  
+  state = fields.Selection([
+    ('draft', 'Draft'),
+    ('in_consultation', 'In Consultation'),
+    ('done', 'Done'),
+    ('cancel', 'Cancel')], default='draft', string='Status', required=True, tracking=True)
+  
+  doctor_id = fields.Many2one('res.users', string='Doctor', tracking=True)
+  pharmacy_line_ids = fields.One2many('apointment.pharmacy.lines', 'appointment_id', string='Pharmacy')
   
   @api.onchange('patient_id')
   def onchange_patient_id(self):
     self.ref = self.patient_id.ref
+    
+  def action_in_cunsultation(self):
+    for rec in self:
+      rec.state = 'in_consultation'
+      
+  def action_done(self):
+    for rec in self:
+      rec.state = 'done'
+      
+  def action_cancel(self):
+    for rec in self:
+      rec.state = 'cancel'
+      
+  def action_draft(self):
+    for rec in self:
+      rec.state = 'draft'
+      
+      
+class AppointmentPharmacyLines(models.Model):
+  _name = "apointment.pharmacy.lines"
+  _description = "Appointment Pharmacy Lines"
+  
+  product_id = fields.Many2one('product.product', required=True)
+  price_unit = fields.Float(related='product_id.list_price')
+  qty = fields.Integer(string='Quantity', default=1)
+  appointment_id = fields.Many2one('hospital.appointment', string='Appointment')
