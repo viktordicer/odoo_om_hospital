06ab069be1cc5ebf094b37a8258210e6b7fbe2da
viktor.dicer <viktor.dicer@gmail.com>
2022-04-27 21:52:08 +0200

    add crate and write def

--- models/patient.py
+++ models/patient.py
@@ -14,6 +14,16 @@ class HospitalPatient(models.Model):
   active = fields.Boolean(string="Active", default=True)
   tag_ids = fields.Many2many('patient.tag', string="Tags")
   
+  @api.model
+  def create(self,vals):
+    vals['ref'] = self.env['ir.sequence'].next_by_code('hospital.patient')
+    return super(HospitalPatient, self).create(vals)
+  
+  def write(self, vals):
+    if not self.ref and not vals.get('ref'):
+      vals['ref'] = self.env['ir.sequence'].next_by_code('hospital.patient')
+    return super(HospitalPatient,self).write(vals)
+  
   @api.depends('date_of_birth')
   def _compute_age(self):
     for rec in self:
