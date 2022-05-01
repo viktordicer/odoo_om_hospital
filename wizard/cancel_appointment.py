ce09ceb98b9d67195fe8a97933a7cf310f0bbaf1
viktor.dicer <viktor.dicer@gmail.com>
2022-05-01 21:18:34 +0200

    add alidation error

--- wizard/cancel_appointment.py
+++ wizard/cancel_appointment.py
@@ -1,15 +1,19 @@
 import datetime
-from odoo import api, fields, models
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
 
 class CancelAppointmentWizard(models.TransientModel):
   _name = "cancel.appointment.wizard"
   _description = "Cancel Appointment Wizard"
   
-  appointment_id = fields.Many2one('hospital.appointment', string="Appointment")
+  appointment_id = fields.Many2one('hospital.appointment', string="Appointment", domain=[('state', '=', 'draft'),
+                                  ('priority', 'in', ('0','1', False))])
   reason = fields.Text(string="Reason")
   date_cancel = fields.Date(string="Cancellation Date")
   
   def action_cancel(self):
+    if self.appointment_id.booking_date == fields.Date.today():
+      raise ValidationError(_("Sorry, you are not allowed to cancel on same date on booking!"))
     return
   
   @api.model
