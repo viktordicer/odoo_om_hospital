1b27cb2998f40a9a19ef276ce093f54732b9de39
viktor.dicer <viktor.dicer@gmail.com>
2022-05-03 22:04:32 +0200

    change rows

--- models/appointment.py
+++ models/appointment.py
@@ -6,8 +6,8 @@ class HospitalAppointment(models.Model):
   _description ="Hospital Appointment"
   #_rec_name = 'ref'
   
-  patient_id = fields.Many2one('hospital.patient', string="Patient")
   name = fields.Char(string='Name', default=lambda self: _('New'))
+  patient_id = fields.Many2one('hospital.patient', string="Patient")
   gender = fields.Selection(related='patient_id.gender')
   appointment_time = fields.Datetime(string='Appointment Time', default=fields.Datetime.now)
   booking_date = fields.Date(string='Booking Date',  default=fields.Date.context_today)
