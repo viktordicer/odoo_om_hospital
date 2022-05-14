b9df81188bc9ff31fc31559eb45ec635bcc79fd9
viktor.dicer <viktor.dicer@gmail.com>
2022-05-14 21:27:35 +0200

    add conffig parameter

--- models/res_config_settings.py
+++ models/res_config_settings.py
@@ -3,4 +3,4 @@ from odoo import fields, models
 class ResConfigSettings(models.TransientModel):
   _inherit = 'res.config.settings'
   
-  cancel_days = fields.Integer(string='Cancel days')
+  cancel_days = fields.Integer(string='Cancel days', config_parameter='om_hospital.cancel_days')
